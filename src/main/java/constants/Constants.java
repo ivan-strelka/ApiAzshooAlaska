51618237ed1456d1696170f3176f361730c63c6f
Ivan.Strelka <ivan.strelka@nordigy.ru>
2022-02-03 12:58:07 +0300

    next

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -2,8 +2,8 @@ package constants;
 
 public class Constants {
 
-    public static final String BASE_URI = "http://161.35.80.61/";
-    public static final Integer PORT = 8090;
+    public static final String BASE_URI = "http://0.0.0.0";
+    public static final Integer PORT = 8091;
 
     public static final class EndPoints {
         public static final String BEAR_GET = "/bear";
